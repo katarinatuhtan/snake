3d478fa61690bd2f8b9404bd5f14d15477287e23
katarinatuhtan <129504867+katarinatuhtan@users.noreply.github.com>
2024-05-22 13:50:58 +0200

    Add files via upload

---
+++ Snake/Snake/Direction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+
+
+        public int RowOffset { get; }
+        public int ColOffsett { get; }
+
+        private Direction(int rowOffset, int colOffsett)
+        {
+            RowOffset = rowOffset;
+            ColOffsett = colOffsett;
+        } 
+        public Direction Opposite()
+        { return new Direction(-RowOffset, -ColOffsett); }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffsett == direction.ColOffsett;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffsett);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ Snake/Snake/Images.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Snake
+{
+    public static class Images
+    {
+        public readonly static ImageSource Empty = LoadImage("Empty.png");
+        public readonly static ImageSource Body = LoadImage("Body.png");
+        public readonly static ImageSource Food = LoadImage("Food.png");
+
+        private static ImageSource LoadImage(string fileName)
+        { return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative)); }
+    }
+}
